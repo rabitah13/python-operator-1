b4b37101a9814bed5e301a5c90cb78136a639323
rabitah13 <pervinrezwana219@gmail.com>
2025-11-15 21:18:33 +0600

    Add files via upload

---
+++ activity2.py
@@ -0,0 +1,7 @@
+amount=int(input("please enter amount for withdraw"))
+note_1=amount//100
+note_2=(amount%100)//50
+note_3=((amount%100)%50)//10
+print("note of 100rupee", note_1)
+print("note of 50rupee", note_2)
+print("note of 10rupee", note_3)
