f7e41f2b42864c4ce2d8c41784572bcb1c5f18bf
rabitah13 <pervinrezwana219@gmail.com>
2025-11-16 13:53:24 +0600

    Add files via upload

--- activity1.py
+++ activity1.py
@@ -1,11 +1,5 @@
-tree1=98
-tree2=94
-tree3=41
-tree4=95
-tree5=11
-sum=tree1+tree2+tree3+tree4+tree5
-print("the sum of all 5 trees are:", sum)
-average=sum/5
-print("the average of all 5 trees are:",average)
-
-
+print("My name is raj")
+print("I am going to invite my friends")
+name = input ("Enter your name")
+birthdaydate = input ("Enter your birthday date")
+print("best of luck")
